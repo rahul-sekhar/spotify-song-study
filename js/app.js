2ba795c11bc2f10045e41fab39da4fc0f9fc3dc9
Rahul Sekhar <sekhar.rahul@gmail.com>
2021-05-27 20:11:31 -0700

    Initial commit - spotify auth

---
+++ js/app.js
@@ -0,0 +1,175 @@
+const CLIENT_ID = "844874dd7c9e407ea670623190f7143e";
+const SCOPES = "user-modify-playback-state";
+const REDIRECT_URI = "http://localhost:8080";
+
+const appConfig = {
+  getVerifier: () => localStorage.getItem("verifier"),
+  setVerifier: (value) => localStorage.setItem("verifier", value),
+  getState: () => localStorage.getItem("state"),
+  setState: (value) => localStorage.setItem("state", value),
+  getAccessToken: () => localStorage.getItem("access_token"),
+  setAccessToken: (value) => localStorage.setItem("access_token", value),
+  getRefreshToken: () => localStorage.getItem("refresh_token"),
+  setRefreshToken: (value) => localStorage.setItem("refresh_token", value),
+};
+
+async function authorizeSpotify(verifier, state) {
+  const challenge = await generateChallenge(verifier);
+
+  const redirectUrl = generateAuthUrl({
+    clientId: CLIENT_ID,
+    redirectUri: REDIRECT_URI,
+    scopes: SCOPES,
+    state,
+    challenge,
+  });
+  window.location.replace(redirectUrl);
+}
+
+function generateAuthUrl(options) {
+  const { clientId, redirectUri, scopes, state, challenge } = options;
+  return `https://accounts.spotify.com/authorize?response_type=code&client_id=${encodeURIComponent(
+    clientId
+  )}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(
+    scopes
+  )}&state=${encodeURIComponent(state)}&code_challenge=${encodeURIComponent(
+    challenge
+  )}&code_challenge_method=S256`;
+}
+
+async function makeTokenRequest(requestData) {
+  const response = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: requestData,
+  });
+  const result = await response.json();
+  appConfig.setAccessToken(result.access_token);
+  appConfig.setRefreshToken(result.refresh_token);
+}
+
+async function requestToken(code) {
+  console.log("Requesting new token");
+
+  const data = new URLSearchParams();
+  data.append("client_id", CLIENT_ID);
+  data.append("grant_type", "authorization_code");
+  data.append("code", code);
+  data.append("redirect_uri", REDIRECT_URI);
+  data.append("code_verifier", appConfig.getVerifier());
+
+  await makeTokenRequest(data);
+}
+
+async function refreshToken() {
+  const data = new URLSearchParams();
+  data.append("client_id", CLIENT_ID);
+  data.append("grant_type", "refresh_token");
+  data.append("refresh_token", appConfig.getRefreshToken());
+
+  await makeTokenRequest(data);
+}
+
+function generateRandomString(length) {
+  const dec2hex = (dec) => ("0" + dec.toString(16)).substr(-2);
+
+  const array = new Uint32Array(length / 2);
+  window.crypto.getRandomValues(array);
+  return Array.from(array, dec2hex).join("");
+}
+
+function sha256(plain) {
+  // returns promise ArrayBuffer
+  const encoder = new TextEncoder();
+  const data = encoder.encode(plain);
+  return window.crypto.subtle.digest("SHA-256", data);
+}
+
+function base64urlencode(a) {
+  var str = "";
+  var bytes = new Uint8Array(a);
+  var len = bytes.byteLength;
+  for (var i = 0; i < len; i++) {
+    str += String.fromCharCode(bytes[i]);
+  }
+  return btoa(str).replace(/\+/g, "-").replace(/\//g, "_").replace(/=+$/, "");
+}
+
+async function generateChallenge(verifier) {
+  hashed = await sha256(verifier);
+  base64encoded = base64urlencode(hashed);
+  return base64encoded;
+}
+
+async function makeSpotifyRequest(url) {
+  const response = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${appConfig.getAccessToken()}`,
+    },
+  });
+
+  const data = await response.json();
+
+  if (
+    !response.ok &&
+    data.error &&
+    data.error.message === "The access token expired"
+  ) {
+    console.log("Spotify token expired");
+    await refreshToken();
+    return makeSpotifyRequest(url);
+  }
+
+  return data;
+}
+
+async function authorizationComplete() {
+  const result = await makeSpotifyRequest("https://api.spotify.com/v1/me");
+
+  console.log(result);
+
+  document.getElementsByClassName(
+    "test-content"
+  )[0].innerHTML = `<p>Hi <a href="${result.external_urls.spotify}" target="_blank">${result.display_name}</a>!</p>`;
+}
+
+async function initialize() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const codeParam = urlParams.get("code");
+  const stateParam = urlParams.get("state");
+
+  if (appConfig.getAccessToken()) {
+    console.log("Existing token found");
+    authorizationComplete();
+  } else if (codeParam) {
+    if (stateParam && stateParam === appConfig.getState()) {
+      try {
+        await requestToken(codeParam);
+        authorizationComplete();
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      console.error(
+        `Invalid state param: ${stateParam}, expected: ${appConfig.getState()}`
+      );
+    }
+  } else {
+    console.log("No token found, initiating authorization");
+    const verifier = generateRandomString(64);
+    appConfig.setVerifier(verifier);
+
+    const state = generateRandomString(10);
+    appConfig.setState(state);
+
+    try {
+      await authorizeSpotify(verifier, state);
+    } catch (error) {
+      console.error("Failed to initiate authorization");
+    }
+  }
+}
+
+initialize();
